f5a691c93b848c3f35094da2cbe6036fa199193d
Alvin Yanson <yansonalvin29@gmail.com>
2021-12-05 17:53:47 +0800

    auto reply with a media based on hashtag

---
+++ bot.js
@@ -0,0 +1,34 @@
+const { auth, postReplyWithMedia, postReply  } = require('./config.js');
+
+const client = auth();
+
+client.stream('statuses/filter', { track: '#FilterMeHashtag' }, function (stream) {
+  console.log("Searching for tweets...");
+
+  // when a tweet is found
+  stream.on('data', function (tweet) {
+    console.log("Found one!");
+    console.log("Recieved tweet reading...", tweet.text);
+
+    // console.log(tweet)
+
+    postReplyWithMedia(client, "./sample-media/video.mp4", tweet);
+
+    // check if tweet contains some media
+    // if (tweet.media_url) {
+    //   console.log("Replying to tweet with video.");
+    //   postReplyWithMedia(client, "./sample-media/video.mp4", tweet);
+
+    // } 
+    
+    // else {
+    //   console.log("Tweet didn't provide media. Replying with message.");
+    //   const message = "Oops, looks like you didn't provide a media file!";
+    //   postReply(client, message, tweet);
+    // }
+
+    stream.on('error', function (error) {
+      console.log(error);
+    });
+  });
+});
